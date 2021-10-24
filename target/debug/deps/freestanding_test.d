568270183f80bdd5aa50ce7190efce2a7294983b
marcos <marcos.caramalho@gmail.com>
2021-10-24 01:34:13 +0100

    Adds gitignore

--- target/debug/deps/freestanding_test.d
+++
@@ -1,5 +0,0 @@
-C:\Users\rezzmk\repos\rust-freestanding\src\freestanding_test\target\debug\deps\freestanding_test.exe: src\main.rs
-
-C:\Users\rezzmk\repos\rust-freestanding\src\freestanding_test\target\debug\deps\freestanding_test.d: src\main.rs
-
-src\main.rs:
--- target/thumbv7em-none-eabihf/debug/freestanding_test.d
+++
@@ -1,1 +0,0 @@
-C:\Users\rezzmk\repos\rust-freestanding\target\thumbv7em-none-eabihf\debug\freestanding_test: C:\Users\rezzmk\repos\rust-freestanding\src\main.rs
